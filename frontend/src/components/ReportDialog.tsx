47f61a3f252963f4ae4fd1a8f03c91f56f4125b8
Wu Jun <wujun4code@gmail.com>
2026-01-07 12:51:50 +0800

    fix: increase z-index of report dialog to prevent being covered by post cards

--- frontend/src/components/ReportDialog.tsx
+++ frontend/src/components/ReportDialog.tsx
@@ -76,7 +76,7 @@ export default function ReportDialog({ postId, commentId, onClose }: ReportDialo
 
     return (
         <div
-            className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 p-4"
+            className="fixed inset-0 bg-black/50 flex items-center justify-center z-[9999] p-4"
             onClick={onClose}
         >
             <div
